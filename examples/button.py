0a3901dbea0fd6fe7f56fdda917afea787917a61
Machine-builder <37518116+Machine-builder@users.noreply.github.com>
2021-04-16 11:16:04 +1000

    Add files via upload

--- examples/button.py
+++ examples/button.py
@@ -10,7 +10,11 @@ pygame = PygameUi.pygame
 Tags = PygameUi.widget_tags.Tags
 
 Widget = PygameUi.Widget
+Container = PygameUi.Container
 Window = PygameUi.Window
+
+ImageWidget = PygameUi.ImageWidget
+Label = PygameUi.Label
 
 
 # pygame and font init ----------------------------------------------------- #
@@ -32,38 +36,64 @@ window_h = 200
 
 window.size = (window_w,window_h)
 window.position = (20,20)
-window.layout_direction = Tags.ROW
+window.layout_direction = Tags.COL
 window.padding = 3
 window.style.border_thickness = 3
 window.style.border_radius = 2
 
-widget_1 = Widget(window)
-widget_1.style.background = (200,150,150)
-widget_1.style.border_thickness = 1
-widget_1.style.border_radius = 2
-widget_1.fluid_size = 1.4,1
-widget_1.margin = 1
+boring_label = Label(None, font, "This is a label")
+boring_label.style.border_thickness = 1
+boring_label.style.border_radius = 2
+boring_label.fluid_size = 1,0.8
+boring_label.margin = 1
 
-widget_2 = Widget(window)
-widget_2.style.background = (150,200,150)
-widget_2.style.border_thickness = 1
-widget_2.style.border_radius = 2
-widget_2.fluid_size = 1,1
-widget_2.margin = 1
+container = Container(window)
+container.style.background = (160,160,160)
+container.style.border_thickness = 1
+container.style.border_radius = 2
+container.fluid_size = 1,1
+container.margin = 1
 
-widget_3 = Widget(window)
-widget_3.style.background = (150,150,200)
-widget_3.style.border_thickness = 1
-widget_3.style.border_radius = 2
-widget_3.fluid_size = 1.8,1
-widget_3.margin = 1
 
-window.add_child(widget_1)
-window.add_child(widget_2)
-window.add_child(widget_3)
+# create and link buttons to functions ------------------------------------- #
 
-# allows any change in size, style, or whatever
-# to reposition the widget's children, and their children too
+cool_button1 = Label(None, font, "print_test1()")
+cool_button1.style.background = (240,240,240)
+cool_button1.style.border_thickness = 2
+cool_button1.style.border_radius = 2
+cool_button1.fluid_size = 3,1
+cool_button1.set_fixed_size(
+    size = (cool_button1.image_size[0]+6, cool_button1.image_size[1]+6),
+    min_margin = (1,1,1,1) # minimum margin of 1px on all sides
+)
+def print_test1(): print("print_test1() function called")
+cool_button1.on_click = print_test1
+
+cool_button2 = Label(None, font, "print_test2()")
+cool_button2.style.background = (240,240,240)
+cool_button2.style.border_thickness = 2
+cool_button2.style.border_radius = 2
+cool_button2.fluid_size = 1,1
+cool_button2.set_fixed_size(
+    size = (cool_button2.image_size[0]+6, cool_button2.image_size[1]+6),
+    min_margin = (1,1,1,1) # minimum margin of 1px on all sides
+)
+def print_test2(): print("print_test2() function called")
+cool_button2.on_click = print_test2
+
+cool_button1.style.margin_align = (Tags.CENTER, Tags.CENTER)
+cool_button2.style.margin_align = (Tags.CENTER, Tags.CENTER)
+
+
+# add widgets to main window and align ------------------------------------- #
+
+container.add_child(cool_button1)
+container.add_child(cool_button2)
+window.add_child(boring_label)
+window.add_child(container)
+
+container.align_fluid_children()
+
 window.auto_reposition_children = True
 
 
--- examples/complex.py
+++ examples/complex.py
@@ -10,7 +10,11 @@ pygame = PygameUi.pygame
 Tags = PygameUi.widget_tags.Tags
 
 Widget = PygameUi.Widget
+Container = PygameUi.Container
 Window = PygameUi.Window
+
+ImageWidget = PygameUi.ImageWidget
+Label = PygameUi.Label
 
 
 # pygame and font init ----------------------------------------------------- #
@@ -37,30 +41,52 @@ window.padding = 3
 window.style.border_thickness = 3
 window.style.border_radius = 2
 
-widget_1 = Widget(window)
-widget_1.style.background = (200,150,150)
-widget_1.style.border_thickness = 1
-widget_1.style.border_radius = 2
-widget_1.fluid_size = 1.4,1
-widget_1.margin = 1
+container_1 = Container(window)
+container_1.style.background = (200,150,150)
+container_1.style.border_thickness = 1
+container_1.style.border_radius = 2
+container_1.fluid_size = 1,1
+container_1.margin = 1
 
-widget_2 = Widget(window)
-widget_2.style.background = (150,200,150)
-widget_2.style.border_thickness = 1
-widget_2.style.border_radius = 2
-widget_2.fluid_size = 1,1
-widget_2.margin = 1
+container_2 = Container(window)
+container_2.style.background = (150,200,150)
+container_2.style.border_thickness = 1
+container_2.style.border_radius = 2
+container_2.fluid_size = 1,1
+container_2.margin = 1
 
-widget_3 = Widget(window)
-widget_3.style.background = (150,150,200)
-widget_3.style.border_thickness = 1
-widget_3.style.border_radius = 2
-widget_3.fluid_size = 1.8,1
-widget_3.margin = 1
+container_3 = Container(window)
+container_3.layout_direction = Tags.COL
+container_3.fluid_size = 1.75,1
+container_3.margin = 1
+container_3.padding = 3
+container_3.style.background = (150,150,200)
+container_3.style.border_thickness = 1
+container_3.style.border_radius = 2
 
-window.add_child(widget_1)
-window.add_child(widget_2)
-window.add_child(widget_3)
+for i in range(5):
+    # add more widgets to this last container
+    list_widget = Container(container_3)
+    list_widget.margin = 2
+    list_widget.style.border_radius = 2
+    list_widget.style.border_colour = (20,20,20)
+    list_widget.style.border_thickness = 2
+    list_widget.style.background = (180,180,180) if (
+        i%2 == 0) else (220,220,220)
+    container_3.add_child(list_widget)
+
+extra_widget = Widget(container_3.child_at(2))
+extra_widget.margin = 3
+extra_widget.style.background = (190,190,230)
+extra_widget.style.border_thickness = 3
+extra_widget.style.border_radius = 2
+
+container_3.child_at(2).add_child(extra_widget)
+container_3.align_fluid_children()
+
+window.add_child(container_1)
+window.add_child(container_2)
+window.add_child(container_3)
 
 # allows any change in size, style, or whatever
 # to reposition the widget's children, and their children too
